ab4e8fd1281f79ed30f2c001798272e7981a869e
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-21 09:28:02 +0200

    create homr view

--- lib/Core/Utils/app_router.dart
+++ lib/Core/Utils/app_router.dart
@@ -1,15 +1,21 @@
 import 'package:go_router/go_router.dart';
+import 'package:music_app/Features/Home/Presentation/Views/home_view.dart';
 import 'package:music_app/Features/Splash/Presentation/Views/splash_view.dart';
 import 'package:music_app/Features/on_boarding/Presentation/Views/on_boaeding_view.dart';
 
 abstract class AppRouter {
   static final kOnBoardingView = "/OnBoardingView";
+  static final kHomeView = "/HomeView";
   static final router = GoRouter(
     routes: [
       GoRoute(path: "/", builder: (context, state) => SplashView()),
       GoRoute(
         path: kOnBoardingView,
         builder: (context, state) => OnBoardingView(),
+      ),
+      GoRoute(
+        path: kHomeView,
+        builder: (context, state) => HomeView(),
       ),
     ],
   );
---
+++ lib/Features/Home/Presentation/Views/home_view.dart
@@ -0,0 +1,16 @@
+
+
+import 'package:flutter/material.dart';
+import 'package:music_app/Core/Widgets/custom_background.dart';
+import 'package:music_app/Features/Home/Presentation/Views/Widgets/home_view_body.dart';
+
+class HomeView extends StatelessWidget {
+  const HomeView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomBackground(child: HomeViewBody()),
+    );
+  }
+}
