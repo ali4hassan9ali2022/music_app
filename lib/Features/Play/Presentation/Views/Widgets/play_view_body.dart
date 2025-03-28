475625f77037e2720b1b7af0e2fdf3f013451303
ali4hassan9ali2022 <kasrawyali47@gmail.com>
2025-03-28 15:05:07 +0200

    create  Custom Songs Details Play

--- lib/Features/Play/Presentation/Views/Widgets/play_view_body.dart
+++ lib/Features/Play/Presentation/Views/Widgets/play_view_body.dart
@@ -1,10 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:music_app/Features/Play/Presentation/Views/Widgets/custom_songs_datails.dart';
 
 class PlayViewBody extends StatelessWidget {
   const PlayViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return const SafeArea(
+      child: Column(children: [SizedBox(height: 98), CustomSongsDetailsPlay(), Row(
+        children: [
+          
+        ],
+      )]),
+    );
   }
 }
